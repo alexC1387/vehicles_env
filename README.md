4e59cdcf52901f4f1d7f2aef379818aee271663d
Alessandra <alessandra.jqr@gmail.com>
2024-08-12 13:16:41 -0400

    se corrigio la estructura de los archivos

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# vehicles_env
+Este conjunto de datos contiene información sobre anuncios de venta de automóviles. Los datos incluyen características clave de las ventas, como el precio, año del modelo, modelo del automóvil, estado, número de cilindros, tipo de combustible, kilometraje (odómetro), tipo de transmisión, color de la pintura, si tiene tracción en las cuatro ruedas (4WD), fecha de publicación y el número de días que estuvo publicado. El objetivo es utilizar estos datos para generar gráficos que representen las características de los automóviles más vendidos.
---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.header("Estadisticas de vehículos vendidos")
+# Leer el archivo CSV en un DataFrame
+df = pd.read_csv('C:/Users/erika/OneDrive/Escritorio/vehicles_env/vehicles_us.csv')
+
+# Muestra el DataFrame en la aplicación Streamlit
+st.write(df)
+        
+car_data = pd.read_csv('C:/Users/erika/OneDrive/Escritorio/vehicles_env/vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón 2
+        
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)       
