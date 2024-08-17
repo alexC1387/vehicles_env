ffa573430f88ded037e59a02861078669fc44c13
Alessandra <alessandra.jqr@gmail.com>
2024-08-16 23:52:38 -0400

    arregle problemas con los datos

--- app.py
+++ app.py
@@ -2,35 +2,31 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Encabezado
-st.header("Estadísticas de vehículos vendidos")
-
+#Encabezado
+st.header("Estadisticas de vehículos vendidos")
 # Leer el archivo CSV en un DataFrame
 df = pd.read_csv('vehicles_us.csv')
+print(df.head())
 
-# Limpiar el DataFrame
-for col in df.columns:
-    if df[col].dtype == 'object':
-        # Rellenar valores NaN con una cadena vacía
-        df[col].fillna('', inplace=True)
-    else:
-        # Rellenar valores NaN con 0 y convertir a float
-        df[col].fillna(0, inplace=True)
-        df[col] = df[col].astype(float)
-
-# Mostrar el DataFrame en la aplicación Streamlit
-st.write(df)
-
-# Botones para construir gráficos
-hist_button = st.button('Construir histograma')
-scatter_button = st.button('Construir gráfico de dispersión')
-
-if hist_button:
+    
+hist_button = st.button('Construir histograma') # crear un botón
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón 2
+       
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+           
+    # crear un histograma
     fig = px.histogram(df, x="odometer")
+       
+    # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-if scatter_button:
+
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
     st.write('Creación de gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
-    fig = px.scatter(df, x="odometer", y="price")
-    st.plotly_chart(fig, use_container_width=True)
+
+
+    fig = px.scatter(df, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)  
